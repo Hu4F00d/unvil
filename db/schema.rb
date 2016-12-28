f448a1d92a36bcd40009442d2e4e7941d7749d86
gabilarregle <gabilarreglemail@gmail.com>
2016-12-28 13:38:10 +0000

    Change for new DB schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,17 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20161226191646) do
 
-  create_table "books", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "name"
-  end
-
   create_table "works", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
