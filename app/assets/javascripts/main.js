5cd94aaa787d5669cf7b422751e084b934fe151f
gabilarregle <gabilarreglemail@gmail.com>
2017-01-04 17:58:06 +0000

    Changed portfolio items order

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,19 +1,4 @@
-$(document).ready(function() {
-    $('.thumbnail-overlay').on('click', function() {
-       $('#myModal').modal(); 
-    });
-    $('#myModal').on('show.bs.modal', function () {
-      console.log("show");
-    })    
-    $('#myModal').on('shown.bs.modal', function () {
-      console.log("shown");
-    })
-    $('#myModal').on('hide.bs.modal', function () {
-      console.log("hide");
-    })      
-    
-})   
-    
+
   // Portfolio JS
   
 	$(function () {
